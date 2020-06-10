107ac8139e759e3710bbb46904ad89ec025a5c5a
nemoto-masashige <nemoto-masashige@dmm.com>
2020-06-10 16:23:54 +0900

    envconfig

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module citest
 
 go 1.14
+
+require github.com/kelseyhightower/envconfig v1.4.0
--- main.go
+++ main.go
@@ -2,11 +2,23 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"citest/container"
+
+	"github.com/kelseyhightower/envconfig"
 )
 
+type Environment struct {
+	Port string `envconfig:"PORT" default:"5000"`
+}
+
 func main() {
+	var vars Environment
+	if err := envconfig.Process("", &vars); err != nil {
+		log.Fatalf("[Error] Failed to process env var: %s", err)
+	}
+
 	c := container.New()
 
 	c.Append(container.Baggage{Name: "test1", Content: "test-content1"})
